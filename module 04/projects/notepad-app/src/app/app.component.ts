cacd69ccd4c51c3c4ab6766a4ed21b2e53e9c5c7
Kobi Hari <kobi2294@yahoo.com>
2023-12-19 15:28:01 +0200

    notepad app

--- module 04/projects/notepad-app/src/app/app.component.ts
+++ module 04/projects/notepad-app/src/app/app.component.ts
@@ -10,5 +10,22 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'notepad-app';
+  colors: string[] = ['red', 'green', 'blue', 'brown', 'black', 'purple'];
+  fonts: string[] = ['Arial', 'David', 'Times new roman', 'Verdana', 'Consolas', 'Segoe UI'];
+  sizes: string[] = ['16px', '18px', '24px', '30px', '42px', '70px'];
+
+  selectedColor: string = this.colors[0];
+  selectedFont: string = this.fonts[0];
+  selectedSize: string = this.sizes[0];
+
+
+  selectColor(value: string) {
+    this.selectedColor = value;
+  }
+  selectFont(value: string) {
+    this.selectedFont = value;
+  }
+  selectSize(value: string) {
+    this.selectedSize = value;
+  }
 }
