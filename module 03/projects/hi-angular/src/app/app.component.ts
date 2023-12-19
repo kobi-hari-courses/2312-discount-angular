00af2a9f9c7e88fc3def42bf8faad0920e456be8
Kobi Hari <kobi2294@yahoo.com>
2023-12-19 11:26:12 +0200

    hello angular - mvvm demo

--- module 03/projects/hi-angular/src/app/app.component.ts
+++ module 03/projects/hi-angular/src/app/app.component.ts
@@ -7,8 +7,32 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'hi-angular';
+  //data
+  keyword: string = '';
+  isBusy: boolean = false;
+  results: string[] = [];
+
+// actions
+  setKeyword(value: string) {
+    this.keyword = value;
+    console.log('setting keyword to ', value);
+  }
+
+  search() {
+    console.log('searching for ', this.keyword);
+    this.isBusy = true;
+    this.results = [];
+
+    setTimeout(() => {
+      this.isBusy = false;
+      this.results = [
+        this.keyword.toLowerCase(), 
+        this.keyword.toUpperCase(), 
+        `*${this.keyword}*`
+      ]
+    }, 3000);
+  }
 }
